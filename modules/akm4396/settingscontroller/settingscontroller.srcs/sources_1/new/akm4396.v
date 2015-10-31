8dfb5cdbd11786e362d8c39170603bf3d64ac824
Yanni <spock@csail.mit.edu>
2015-10-31 18:42:38 -0400

    added crappy block diagram

---
+++ modules/akm4396/settingscontroller/settingscontroller.srcs/sources_1/new/akm4396.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/31/2015 06:11:49 PM
+// Design Name: 
+// Module Name: akm4396
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module akm4396controller(
+input wire serialclk,
+input wire reset
+    );
+endmodule
